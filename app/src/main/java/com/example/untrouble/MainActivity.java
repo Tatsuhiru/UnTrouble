27f6bda8bfa1dce7107faeb7529ccd2fcb07c9ea
Tatsu <81825673+Tatsuhiru@users.noreply.github.com>
2023-10-13 13:56:47 +0700

    mengoptimalkan source code
    membuat kode sedikit lebih rapi dan efisien

--- app/src/main/java/com/example/untrouble/MainActivity.java
+++ app/src/main/java/com/example/untrouble/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.untrouble;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AlertDialog;
@@ -21,15 +22,15 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.home);
-        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             switch (item.getItemId()) {
                 case R.id.search:
-                    startActivity(new Intent(getApplicationContext(), SearchActivity.class));
+                    startActivity(new Intent(this, SearchActivity.class));
                     overridePendingTransition(0,0);
                     return true;
                 case R.id.reminder:
-                    startActivity(new Intent(getApplicationContext(), com.example.untrouble.ReminderActivity.class));
+                    startActivity(new Intent(this, ReminderActivity.class));
                     overridePendingTransition(0,0);
                     return true;
                 case R.id.home:
@@ -39,41 +40,31 @@ public class MainActivity extends AppCompatActivity {
             return false;
         });
 
-        final MediaPlayer buttonSound = MediaPlayer.create(this, R.raw.button_sound);
-
-        Button button1 = findViewById(R.id.buttontutorial);
-        button1.setOnClickListener(v -> {
-            buttonSound.start();
-            openTutorial();
-        });
-
-        Button button2 = findViewById(R.id.buttonabout);
-        button2.setOnClickListener(v -> {
-            openAbout();
-            buttonSound.start();
-        });
-
-        Button button3 = findViewById(R.id.buttonexit);
-        button3.setOnClickListener(v -> {
-            onBackPressed();
-        });
+        findViewById(R.id.buttonquiz).setOnClickListener(this::openQuiz);
+        findViewById(R.id.buttonabout).setOnClickListener(this::openAbout);
+        findViewById(R.id.buttonexit).setOnClickListener(view -> onBackPressed());
     }
 
-    private void openAbout() {
-        Intent intent = new Intent(this, com.example.untrouble.about.class);
-        startActivity(intent);
+    private void openQuiz(View view) {
+        startActivity(new Intent(this, Quiz.class));
+        playButtonSound();
     }
 
-    public void openTutorial(){
-        Intent intent = new Intent(this, com.example.untrouble.Quiz.class);
-        startActivity(intent);
+    private void openAbout(View view) {
+        startActivity(new Intent(this, About.class));
+        playButtonSound();
+    }
+
+    private void playButtonSound() {
+        MediaPlayer buttonSound = MediaPlayer.create(this, R.raw.button_sound);
+        buttonSound.start();
     }
 
     @Override
-    public void onBackPressed(){
-        final MediaPlayer errorSound = MediaPlayer.create(this, R.raw.error_sound);
-        final MediaPlayer buttonSound = MediaPlayer.create(this, R.raw.button_sound);
+    public void onBackPressed() {
+        MediaPlayer errorSound = MediaPlayer.create(this, R.raw.error_sound);
         errorSound.start();
+
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Alert!!");
         builder.setIcon(android.R.drawable.stat_sys_warning);
@@ -81,14 +72,12 @@ public class MainActivity extends AppCompatActivity {
         builder.setPositiveButton("YES", (dialog, which) -> {
             finishAffinity();
             System.exit(0);
-            buttonSound.start();
+            playButtonSound();
         });
         builder.setNegativeButton("NO", (dialog, which) -> {
             dialog.cancel();
-            buttonSound.start();
+            playButtonSound();
         });
-        AlertDialog alertDialog=builder.create();
-        alertDialog.show();
+        builder.create().show();
     }
-
 }
